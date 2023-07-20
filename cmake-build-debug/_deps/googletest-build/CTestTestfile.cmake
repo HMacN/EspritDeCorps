f1d37f62a79a7e83f7e2e1fb02994c1f81d5e70e
HMacN <hugh.macn@gmail.com>
2023-07-20 02:00:44 +0100

    Setup - Imported Google Test.
    * Imported the Google Test framework.
    
    * Preliminary setup work to get the project to compile and run with the testing framework.

---
+++ cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/hughm/Desktop/Written_Code/EspritDeCorps/cmake-build-debug/_deps/googletest-src
+# Build directory: C:/Users/hughm/Desktop/Written_Code/EspritDeCorps/cmake-build-debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/hughm/Desktop/Written_Code/EspritDeCorps/cmake-build-debug/_deps/googletest-src/googlemock
+# Build directory: C:/Users/hughm/Desktop/Written_Code/EspritDeCorps/cmake-build-debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
